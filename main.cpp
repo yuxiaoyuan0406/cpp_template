44a964e3560cb5dca7a6d330689753e19063806d
yuxiaoyuan0406 <973971990@qq.com>
2022-09-08 09:09:14 +0800

    Merge branch 'vscode' into master

--- main.cpp
+++ main.cpp
@@ -1,24 +1,39 @@
 #include <cstdio>
 #include <thread>
 
-void left_hand()
-{
-    putchar('a');
-}
+// void left_hand()
+// {
+//     putchar('a');
+// }
 
-void right_hand()
+// void right_hand()
+// {
+//     putchar('m');
+//     putchar('i');
+//     putchar('n');
+// }
+
+void hand(char *s)
 {
-    putchar('m'); 
-    putchar('i');
-    putchar('n');
+    for (; *s; s++)
+    {
+        putchar(*s);
+    }
 }
 
 int main(int argc, char **argv)
 {
-    std::thread right_hand_thread(right_hand);
-    std::thread left_hand_thread(left_hand);
+    if (argc < 3)
+    {
+        fprintf(stderr, "too less arguments\n");
+        return 0;
+    }
+    std::thread right_hand_thread([argv]()
+                                  { hand(argv[1]); });
+    std::thread left_hand_thread([argv]()
+                                  { hand(argv[2]); });
     left_hand_thread.join();
     right_hand_thread.join();
-   
+
     return 0;
 }
