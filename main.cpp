28a406fecd74d34da5207ef252a1b6d2ff6b1b99
DennisYu <973971990@qq.com>
2022-11-21 06:40:38 +0000

    use astyle to format code in linux style

--- main.cpp
+++ main.cpp
@@ -15,23 +15,23 @@
 
 void hand(char *s)
 {
-    for (; *s; s++)
-    {
+    for (; *s; s++) {
         putchar(*s);
     }
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
-    {
+    if (argc < 3) {
         fprintf(stderr, "too less arguments\n");
         return 0;
     }
-    std::thread right_hand_thread([argv]()
-                                  { hand(argv[1]); });
-    std::thread left_hand_thread([argv]()
-                                  { hand(argv[2]); });
+    std::thread right_hand_thread([argv]() {
+        hand(argv[1]);
+    });
+    std::thread left_hand_thread([argv]() {
+        hand(argv[2]);
+    });
     left_hand_thread.join();
     right_hand_thread.join();
 
